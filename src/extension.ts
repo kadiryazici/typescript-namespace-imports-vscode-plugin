6adaa08c7657d7168627ac585481faf414df0404
kadiryazici <kyzc411@gmail.com>
2026-03-18 02:48:52 +0300

    remove gql

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import { CompletionItemsCache } from "./completion_items_cache";
 import { CompletionItemsCacheImpl } from "./completion_items_cache_impl";
 import { resolveCompletionItemDetails } from "./uri_helpers";
 
-const openGraphQLTag = /gql`[^`]*$/;
 
 export function activate(context: vscode.ExtensionContext) {
     const workspaceFolders = vscode.workspace.workspaceFolders;
@@ -65,9 +64,6 @@ export function activate(context: vscode.ExtensionContext) {
             provideCompletionItems(doc: vscode.TextDocument, position: vscode.Position) {
                 const wordRange = doc.getWordRangeAtPosition(position);
                 if (!wordRange) return new vscode.CompletionList([], true);
-                // Don't provide completions if the cursor is inside a gql`` template literal to
-                // avoid conflicting with fragment name completions from the GraphQL extension.
-                if (isInGraphQLTag(doc, position)) return new vscode.CompletionList([], true);
 
                 const word = doc.getText(wordRange);
                 return moduleCompletionItemsCache.getCompletionList(doc, word);
@@ -84,28 +80,3 @@ export function activate(context: vscode.ExtensionContext) {
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export function deactivate() {}
 
-/**
- * Returns an approximation for whether the cursor is inside a gql`` template literal by searching
- * backwards in the document. Returns true if "gql`" is encountered before a standalone "`" or
- * a semicolon.
- */
-function isInGraphQLTag(doc: vscode.TextDocument, position: vscode.Position): boolean {
-    const textBeforeCursor = doc.lineAt(position.line).text.slice(0, position.character);
-    if (openGraphQLTag.test(textBeforeCursor)) {
-        return true;
-    }
-    if (textBeforeCursor.includes("`") || textBeforeCursor.includes(";")) {
-        return false;
-    }
-    const scanLimit = Math.max(0, position.line - 100);
-    for (let i = position.line - 1; i >= scanLimit; i--) {
-        const line = doc.lineAt(i).text;
-        if (openGraphQLTag.test(line)) {
-            return true;
-        }
-        if (line.includes("`") || line.includes(";")) {
-            return false;
-        }
-    }
-    return false;
-}
